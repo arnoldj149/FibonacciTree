67883e45fd87bb08b9fe04481f1b3ee1d5066c99
jra2196 <arnoldj149@gmail.com>
2014-05-27 20:05:21 -0400

    It took some thought, but I know how I want to build the tree now. I decided to let the tree itself manage all of the nodes itself since the nature of the tree is that it does not need to be altered after creation. All of the nodes are held in an array of arrays in the FibonacciTree class and can be accessed through arithmetic, but the ability to traverse the nodes like a tree has been left in for the sake of being "A tree" of integers. Things I have in this commit: -Created members and accessors for FibonacciTree including an array of node arrays the tree owns, an accessor for the root of the tree, and a prefab to use as nodes. THE GAMEOBJECT USED AS THE NODEPREFAB MUST HAVE A FIBONACCINODE SCRIPT COMPONENT! -Created BuildTree function in FibonacciTree that generates all of the nodes for the tree based on a depth supplied as a parameter. -At this point, BuildTree is getting called on the start of the FibonacciTree MonoBehavior for testing purposes. Later, it will be called when a level is supplied by the user. -Added a FibonacciNode.cs script representing a node of the tree. The GameObject that is used as the nodePrefab in the tree has this script as a component. -Created members and accessors for FibonacciNode including an integer data member, a left child FibonacciNode member, and a right child FibonacciNode member. -Created a function in FibonacciNode called CreateChildren that will take some of the code out of BuildTree so that parents create their children.

---
+++ FibonacciTree/Assets/My Assets/Scripts/FibonacciNode.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A class representing a node in a FibonacciTree.
+/// </summary>
+public class FibonacciNode : MonoBehaviour {
+
+#region Members & Accessors
+	/// <summary>
+	/// The integer data held in the node.
+	/// </summary>
+	private int data;
+	/// <summary>
+	/// Accessor for data.
+	/// </summary>
+	/// <value>The new data.</value>
+	public int Data
+	{
+		get { return data; }
+		set { data = value; }
+	}
+	/// <summary>
+	/// The left child node of this node.
+	/// </summary>
+	private FibonacciNode left;
+	/// <summary>
+	/// Accessor for the left child.
+	/// </summary>
+	/// <value>The new left child.</value>
+	public FibonacciNode Left
+	{
+		get { return left; }
+		set { left = value; }
+	}
+
+	/// <summary>
+	/// The right child node of this node.
+	/// </summary>
+	private FibonacciNode right;
+	/// <summary>
+	/// Accessor for the right child.
+	/// </summary>
+	/// <value>The new right child.</value>
+	public FibonacciNode Right
+	{
+		get { return right; }
+		set { right = value; }
+	}
+#endregion
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Creates the children of this node with supplied data values.
+	/// </summary>
+	/// <param name="leftData">Data to give the left child.</param>
+	/// <param name="rightData">Data to give the right child.</param>
+	void CreateChildren(int leftData, int rightData)
+	{
+		//will probably take some parts out of the BuildTree function in FibonacciTree
+		//and move them here so that child nodes are created by their parents.
+	}
+}
--- FibonacciTree/Assets/My Assets/Scripts/FibonacciTree.cs
+++ FibonacciTree/Assets/My Assets/Scripts/FibonacciTree.cs
@@ -1,15 +1,148 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// A tree that follows a set of rules similar to that of the Fibonacci Sequence.
+/// </summary>
 public class FibonacciTree : MonoBehaviour {
+
+	/// <summary>
+	/// The GameObject prefab to use as nodes for the tree. 
+	/// PREFAB MUST HAVE A "FibonacciNode.cs" SCRIPT COMPONENT ATTATCHED!!!
+	/// </summary>
+	public GameObject nodePrefab;
+
+	/// <summary>
+	/// The nodes for the tree. Since the nature of this tree is that it is created based on a series
+	/// of rules rather than through something more random like user input, the entire tree can be
+	/// created as soon as a depth is specified by the user. We can take advantage of that fact and 
+	/// maintain all of the tree's data in a structured series of arrays that can be traversed 
+	/// </summary>
+	private FibonacciNode[][] nodes;
+
+	/// <summary>
+	/// Gets the root node of the tree.
+	/// </summary>
+	/// <value>The root.</value>
+	public FibonacciNode Root
+	{
+		get { return nodes[0][0]; }
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		//Test by building the tree here rather than through user input.
+		BuildTree(5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
+
+	/// <summary>
+	/// Builds a tree from a depth supplied by by the user using these rules:
+	/// 1)The root has a value of 1. 
+	/// 2)Each node has two children. 
+	/// 3)If it's the left child, its value will be the sum of the parent's value and the parent's 
+	/// left sibling value. If the parent has no left sibling, then the child's value is the 
+	/// same as its parent.
+	/// 4)If it's the right child, its value will be the sum of the parent's value and the parent's 
+	/// right sibling value. If the parent has no right sibling, then the child's value is the 
+	/// same as its parent.
+	/// </summary>
+	/// <param name="depth">The number of generations to create in the tree. A depth of 1 will 
+	/// create only the root. If the depth is less than 1, the tree is not constructed.</param>
+	void BuildTree(uint depth)
+	{
+		//if depth is less than 1, no tree is generated.
+		if (depth < 1)
+			return;
+
+		//the number of siblings at the lowest level of this tree (Used to determine positioning of nodes).
+		float maxSiblings = Mathf.Pow (2,depth-1);
+
+		//otherwise create a temporary array of FibonacciNode arrays large enough for 'depth' number of generations.
+		nodes = new FibonacciNode[depth][];
+
+		//now generate data for each level of the tree:
+
+		int siblings;
+		int parentIndex;
+		//for each level of the tree...
+		for (int i = 0; i < depth; i++)
+		{
+			//there are 2^n siblings per generation where n is the generation's level (if root is level 0).
+			siblings = (int) Mathf.Pow(2,i);
+			//allocate memory for this level of the tree large enough to hold that many siblings.
+			nodes[i] = new FibonacciNode[siblings];
+
+			//if this is the root, treat it differently
+			if (i == 0)
+			{
+				//instatiate a node prefab for the root node and cast it as a FibonacciNode.
+				nodes[i][0] = ((GameObject)GameObject.Instantiate(nodePrefab, Vector3.zero, Quaternion.identity)).GetComponent<FibonacciNode>();
+
+				//the root is given the value 1.
+				nodes[i][0].Data = 1;
+			}
+			//otherwise...
+			else
+			{
+				//for each pair of children in this generation...
+				for (int j = 0; j < siblings; j += 2)
+				{
+					//find the index of the parent in generation i - 1 for these two children.
+					parentIndex = j / 2;
+
+					//FibonacciNode parent = nodes[i][j].GetComponent<FibonacciNode>();
+
+					//instatiate a node prefab for the left child node and cast it as a FibonacciNode.
+					nodes[i][j] = ((GameObject)Instantiate(nodePrefab, 
+											nodes[i-1][parentIndex].transform.position + new Vector3(-maxSiblings / siblings,0,2), 
+					                                       Quaternion.identity)).GetComponent<FibonacciNode>();
+					//instatiate a node prefab for the right child node and cast it as a FibonacciNode.
+					nodes[i][j+1] = ((GameObject)Instantiate(nodePrefab, 
+					                        nodes[i-1][parentIndex].transform.position + new Vector3(maxSiblings / siblings,0,2), 
+						                                           Quaternion.identity)).GetComponent<FibonacciNode>();
+					
+					//set the left and right children of the parent to the new node objects.
+					nodes[i-1][parentIndex].Left = nodes[i][j];
+					nodes[i-1][parentIndex].Right = nodes[i][j+1];
+
+					//find data for left child based on previous generation.
+					if (j > 0)
+					{
+						//if these are not the left most children in the generation, then the left
+						//child's data is equal to its parent's data + the parent's left sibling's data.
+						nodes[i][j].Data = nodes[i-1][parentIndex-1].Data + nodes[i-1][parentIndex].Data;
+					}
+					else
+					{
+						//otherwise the left child's data is equal to the parent's data.
+						nodes[i][j].Data = nodes[i-1][parentIndex].Data;
+					}
+
+					//find data for right child based on previous generation.
+					if (j < siblings - 2)
+					{
+						//if these are not the right most children in the generation, then the right
+						//child's data is equal to its parent's data + the parent's right sibling's data.
+						nodes[i][j+1].Data = nodes[i-1][parentIndex].Data + nodes[i-1][parentIndex+1].Data;
+					}
+					else
+					{
+						//otherwise the right child's data is equal to the parent's data.
+						nodes[i][j+1].Data = nodes[i-1][parentIndex].Data;
+					}
+				}
+			}
+		}
+
+		//Now Instatiate GameObjects to represent the tree visually
+
+
+	}
+
+
 }
